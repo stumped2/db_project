e472b65bd1e402c7cb28cc41b152dde24edc4a4c
Geoffrey Corey <coreyg@onid.orst.edu>
2012-12-23 15:19:34 -0800

    Removed CSS from the php/html file, moving to a dedicated css file

--- db_project/index.php
+++ db_project/index.php
@@ -1,103 +1,5 @@
 <!DOCTYPE html>
 <html lang=en>
-
-<style>
-#head-container {
-	color: #000;
-	background: #ccc;
-}
-
-#header {
-	margin: 0 auto;
-	width: 860px;
-	padding: 20px;
-	background: #ddd;
-}
-
-#header h1 {
-	margin: 0;
-}
-
-#navigation-container {
-	float: left;
-	width: 100%;
-	color: #000;
-	background: #333;
-}
-
-#navigation {
-	margin: 0 auto;
-	width: 900px;
-}
-
-#navigation ul {
-	margin: 0;
-	padding: 0;
-}
-
-#navigation ul li {
-	list-style-type: none;
-	display: inline;
-}
-
-#navigation li a {
-	display: block;
-	float: left;
-	padding: 5px 10px;
-	color: #fff;
-	text-decoration: none;
-	border-right: 1px solid #fff;
-}
-
-#navigation li a:hover {
-	background: #383;
-}
-
-#content-container {
-	float: left;
-	width: 100%;
-	color: #000;
-	background: #eee;
-}
-
-#content-container2 {
-	margin: 0 auto;
-	width: 900px;
-}
-
-#content-container3 {
-	float: left;
-	width: 900px;
-	background: #FFF url(two-fixed-background.gif) repeat-y 100% 0;
-}
-
-#content {
-	clear: left;
-	float: left;
-	padding: 20px 0;
-	margin: 0 0 0 30px;
-	display: inline;
-}
-
-#content h2 { margin: 0; }
-
-#footer-container
-{
-	clear: left;
-	color: #fff;
-	background: #000;
-}
-
-#footer
-{
-	margin: 0 auto;
-	width: 900px;
-	text-align: right;
-	padding: 20px;
-	height: 1%;
-}
-</style>
-
 <body>
 	<div id="head-container">
 		<div id="header">
@@ -126,7 +28,7 @@
 					<p>Use the navigation ribbon above to nativate through the database 
 					   controls.</p>
 					   
-					<p>Please contact the webmaster, Geoffrey Corey, at yourapanzy91@gmail.com
+					<p>Please contact the webmaster <a href= "mailto:yourapanzy91@gmail.com">Geoffrey Corey</a>
 					   if you encouter any issues.</p>
 				</div>
 
