6b792863e98d8b2b0a1391276b4b783ea13aa997
Geoffrey Corey <coreyg@onid.orst.edu>
2012-12-23 16:57:47 -0800

    Added initial stubs for the pages. Need to fix each page for appropriate navigation values as well as modify index.php for appropriate navigation. Also working on client-side form evaluation.

--- db_project/index.php
+++ db_project/index.php
@@ -9,10 +9,10 @@
 	<div id="navigation-container">
 		<div id="navigation">
 			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="views/users.php">Users Statistics</a></li>
-				<li><a href="#">Storage Information</a></li>
-				<li><a href="#">Tier Information</a></li>
+				<li><a href="input/userin.php">Add User</a></li>
+				<li><a href="views/userview.php">User Information</a></li>
+				<li><a href="#">Storage Statistics</a></li>
+				<li><a href="#">Tier Statistics</a></li>
 			</ul>
 		</div>
 	</div>
@@ -22,14 +22,13 @@
 				<div id="content">
 					<h2>Weclome!</h2>
 					
-					<p>This website will allow you to add, remove, update, and renew
-					   users of Cloud Services Inc. services.</p>
+					<p>This page is for managing subscribers of
+					   Cloud Storage Services. You will be able to add or remove users,
+					   modify account standing of users andmodify the subscription details
+					   associated with the user.</p>
 					
-					<p>Use the navigation ribbon above to nativate through the database 
-					   controls.</p>
+					<p>Use the navigation ribbon above to go to where you need.</p>
 					   
-					<p>Please contact the webmaster <a href= "mailto:yourapanzy91@gmail.com">Geoffrey Corey</a>
-					   if you encouter any issues.</p>
 				</div>
 
 			</div>
@@ -37,6 +36,9 @@
 		<div id="footer-container">
 			<div id="footer">
 				Copyright © Cloud Services Inc., 2012 
+				<br>
+				Please contact the webmaster <a href= "mailto:yourapanzy91@gmail.com">yourapanzy91@gmail.com</a>
+					   if you encouter any issues.
 			</div>
 		</div>
 	</div>
---
+++ db_project/input/userin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang=en>
+<body>
+	<div id="head-container">
+		<div id="header">
+			<h1>Cloud Storage Services Inc.</h1>
+		</div>
+	</div>
+	<div id="navigation-container">
+		<div id="navigation">
+			<ul>
+				<li><a href="../index.php">Home</a></li>
+				<li><a href="views/userview.php">Users Statistics</a></li>
+				<li><a href="#">Storage Information</a></li>
+				<li><a href="#">Tier Information</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="content-container">
+		<div id="content-container2">
+			<div id="content-container3">
+				<div id="content">
+					<h2>Add User to Database</h2>
+					   
+					<p>Please contact the webmaster <a href= "mailto:yourapanzy91@gmail.com">Geoffrey Corey</a>
+					   if you encouter any issues.</p>
+				</div>
+
+			</div>
+		</div>
+		<div id="footer-container">
+			<div id="footer">
+				Copyright © Cloud Services Inc., 2012 
+			</div>
+		</div>
+	</div>
+</body>
+</html>
